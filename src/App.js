516bcc7526ee7a037a91882627a0a0945d2877fa
Chris Lee <dkrj45@gmail.com>
2022-09-15 18:46:34 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//axios installed as well :)
+import './App.scss';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import PlayerPage from './pages/UploadPage/UploadPage';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Navigate to='/Home' />}></Route>
+            <Route path='/players/:name' element={<PlayerPage />}></Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
     </div>
   );
 }
