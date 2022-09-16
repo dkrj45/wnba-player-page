18f9a09ed60f243f7faa199056a35fee0a20e29f
Abrar <abrarfahimr@gmail.com>
2022-09-16 00:22:33 -0400

    made hero section

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -5,6 +5,12 @@ import tiktokIcon from '../../assets/icons/Vector-1.svg';
 import facebookIcon from '../../assets/icons/Vector-2.svg';
 import snapchatIcon from '../../assets/icons/Vector-3.svg';
 import twitterIcon from '../../assets/icons/Vector-4.svg';
+import teamIcon from '../../assets/icons/team-icon.svg';
+import heroImage from '../../assets/images/player-image.png';
+import defaultMedal from '../../assets/icons/default-medal.svg';
+import variant1Medal from '../../assets/icons/variant1-medal.svg';
+import variant2Medal from '../../assets/icons/variant2-medal.svg';
+
 
 const HeroImage = () => {
   return (
@@ -57,8 +63,49 @@ const HeroImage = () => {
           </a>
         </div>
       </div>
-      <div className="heroimage__">
-
+      <div className="heroimage__mainsection">
+        <div className="heroimage__description">
+          <div className="heroimage__intro">
+            <h2 className="heroimage__number">#3</h2>
+            <h1 className="heroimage__name">
+              Diana <br />{' '}
+              <span className="heroimage__secondname">TAURASI</span>
+            </h1>
+            <h2 className="heroimage__title">Guard</h2>
+          </div>
+          <p className="heroimage__team">
+            <img
+              className="heroimage__teamicon"
+              src={teamIcon}
+              alt="mercury pheonix"
+            ></img>
+            Mercury Phoenix
+          </p>
+        </div>
+        <div className="heroimage__player">
+          <img
+            src={heroImage}
+            className="heroimage__heroimage"
+            alt="Diana Taurasi"
+          ></img>
+        </div>
+        <div className="heroimage__medal" alt="medals">
+          <img
+            className="heroimage__medal1"
+            src={defaultMedal}
+            alt="default medal"
+          ></img>
+          <img
+            className="heroimage__medal1"
+            src={variant1Medal}
+            alt="variant 1 medal"
+          ></img>
+          <img
+            className="heroimage__medal1"
+            src={variant2Medal}
+            alt="variant 2 medal"
+          ></img>
+        </div>
       </div>
     </main>
   );
