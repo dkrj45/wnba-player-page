3672fd793d7128b8934f9db3c344c767f0effaa7
John Bosley <johnbosley@Johns-MacBook-Air.local>
2022-09-16 11:35:37 -0400

    offcourt

--- src/pages/PlayerPage/PlayerPage.js
+++ src/pages/PlayerPage/PlayerPage.js
@@ -1,11 +1,12 @@
 //axios installed as well :)
 import HeroImage from "../../components/HeroImage/HeroImage";
-
+import OffCourt from "../../components/OffCourt/OffCourt";
 function PlayerPage() {
 
   return (
     <div className="player-page">
       <HeroImage />
+      <OffCourt />
     </div>
   );
 }
