01fed42a523be11893c7ec5b028bdfbed0bf4dfe
Abrar <abrarfahimr@gmail.com>
2022-09-15 22:49:43 -0400

    initial branch setup

---
+++ src/components/HeroImage/HeroImage.js
@@ -0,0 +1,12 @@
+import './HeroImage.scss';
+
+const HeroImage = () => {
+  return (
+    <main className="heroimage">
+      <div className="heroimage__iconleft"></div>
+      <div className="heroimage__iconright"></div>
+    </main>
+  )
+}
+
+export default HeroImage
