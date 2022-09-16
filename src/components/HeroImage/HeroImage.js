87c8209eb2547bc4ca0bb431f6a8d13584ecaabb
Chris Lee <dkrj45@gmail.com>
2022-09-16 02:46:57 -0400

    .

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -14,60 +14,61 @@ import variant2Medal from '../../assets/icons/variant2-medal.svg';
 
 const HeroImage = () => {
   return (
-    <main className="heroimage">
-      <div className='heroimage__wrapper'>
-        <div className="heroimage__description">
-          <div className="heroimage__intro">
-            <h2 className="heroimage__number">#3</h2>
-            <p className="heroimage__team">
-              <img
-                className="heroimage__teamicon"
-                src={teamIcon}
-                alt="mercury pheonix"
-              ></img>
-              Mercury Phoenix
-            </p>
-            <h1 className="heroimage__name">
-              Diana<span className="heroimage__secondname">TAURASI</span>
-            </h1>
-            <h2 className="heroimage__title">Guard</h2>
+    <main className='hero'>
+      <div className='heroimage'>
+        <div className='heroimage__wrapper'>
+          <div className="heroimage__description">
+            <div className="heroimage__intro">
+              <h2 className="heroimage__number">#3</h2>
+              <p className="heroimage__team">
+                <img
+                  className="heroimage__teamicon"
+                  src={teamIcon}
+                  alt="mercury pheonix"
+                ></img>
+                Mercury Phoenix
+              </p>
+              <h1 className="heroimage__name">
+                Diana<span className="heroimage__secondname">TAURASI</span>
+              </h1>
+              <h2 className="heroimage__title">Guard</h2>
+            </div>
           </div>
-        </div>
-        <div className="heroimage__linkicons">
-          <div className="heroimage__iconright">
-            <a href="https://www.instagram.com/dianataurasi/" className="heroimage__link">
-              <img
-                src={instaIcon}
-                alt="right-icon"
-                className="heroimage__instagram"
-              ></img>
-            </a>
-            <a href="https://www.tiktok.com/@dianataurasiofficial?lang=en" className="heroimage__link">
-              <img
-                src={tiktokIcon}
-                alt="right-icon"
-                className="heroimage__instagram"
-              ></img>
-            </a>
-            <a href="https://www.facebook.com/dianataurasi" className="heroimage__link">
-              <img
-                src={facebookIcon}
-                alt="right-icon"
-                className="heroimage__instagram"
-              ></img>
-            </a>
-            <a href="https://twitter.com/DianaTaurasi" className="heroimage__link">
-              <img
-                src={twitterIcon}
-                alt="right-icon"
-                className="heroimage__instagram"
-              ></img>
-            </a>
-          </div>
-          <div className="heroimage__iconleft">
-            <button className="heroimage__bell"><img className='heroimage__bell-image' src={leftIcon} />Follow</button>
-          </div>
-          <div className="heroimage__medal" alt="medals">
+          <div className="heroimage__linkicons">
+            <div className="heroimage__iconright">
+              <a href="https://www.instagram.com/dianataurasi/" className="heroimage__link">
+                <img
+                  src={instaIcon}
+                  alt="right-icon"
+                  className="heroimage__instagram"
+                ></img>
+              </a>
+              <a href="https://www.tiktok.com/@dianataurasiofficial?lang=en" className="heroimage__link">
+                <img
+                  src={tiktokIcon}
+                  alt="right-icon"
+                  className="heroimage__instagram"
+                ></img>
+              </a>
+              <a href="https://www.facebook.com/dianataurasi" className="heroimage__link">
+                <img
+                  src={facebookIcon}
+                  alt="right-icon"
+                  className="heroimage__instagram"
+                ></img>
+              </a>
+              <a href="https://twitter.com/DianaTaurasi" className="heroimage__link">
+                <img
+                  src={twitterIcon}
+                  alt="right-icon"
+                  className="heroimage__instagram"
+                ></img>
+              </a>
+            </div>
+            <div className="heroimage__iconleft">
+              <button className="heroimage__bell"><img className='heroimage__bell-image' src={leftIcon} />Follow</button>
+            </div>
+            <div className="heroimage__medal" alt="medals">
               <img
                 className="heroimage__medal1"
                 src={defaultMedal}
@@ -84,6 +85,7 @@ const HeroImage = () => {
                 alt="variant 2 medal"
               ></img>
             </div>
+          </div>
         </div>
       </div>
     </main>
